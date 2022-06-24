7993de7a386f089a4cd2a785e6516c961ac22786
Lucia Calabria <luciacalabria80@gmail.com>
2022-06-24 16:49:10 +0200

    Creazione Utente e Ruolo service

---
+++ src/main/java/it/prova/pockeronline/service/RuoloServiceImpl.java
@@ -0,0 +1,45 @@
+package it.prova.pockeronline.service;
+
+import java.util.List;
+
+import it.prova.pockeronline.model.Ruolo;
+
+public class RuoloServiceImpl implements RuoloService{
+
+	@Override
+	public List<Ruolo> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Ruolo caricaSingoloElemento(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void aggiorna(Ruolo ruoloInstance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void inserisciNuovo(Ruolo ruoloInstance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rimuovi(Ruolo ruoloInstance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Ruolo cercaPerDescrizioneECodice(String descrizione, String codice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/it/prova/pockeronline/service/UtenteServiceImpl.java
@@ -0,0 +1,75 @@
+package it.prova.pockeronline.service;
+
+import java.util.List;
+
+import it.prova.pockeronline.model.Utente;
+
+public class UtenteServiceImpl implements UtenteService {
+
+	@Override
+	public List<Utente> listAllUtenti() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utente caricaSingoloUtente(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utente caricaSingoloUtenteConRuoli(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void aggiorna(Utente utenteInstance) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void inserisciNuovo(Utente utenteInstance) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rimuovi(Utente utenteInstance) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Utente> findByExample(Utente example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utente findByUsernameAndPassword(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utente eseguiAccesso(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void changeUserAbilitation(Long utenteInstanceId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Utente findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
